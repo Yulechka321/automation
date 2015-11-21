a966f7577979d23312d51d04b96cb5a57d719b2b
Yulia Alipova <Yulia Alipova>
2015-11-21 13:16:31 -0500

    first commit

---
+++ readmefile.js
@@ -0,0 +1,6 @@
+echo # automation >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Yulechka321/automation.git
+git push -u origin master
