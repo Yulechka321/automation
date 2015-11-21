50b10efdf906451d15f061712c2942b7f9308a1c
Yulia Alipova <Yulia Alipova>
2015-11-21 13:37:34 -0500

    deleded commit

--- readmefile.js
+++
@@ -1,6 +0,0 @@
-echo # automation >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/Yulechka321/automation.git
-git push -u origin master
