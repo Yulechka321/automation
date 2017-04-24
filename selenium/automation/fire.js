56c651322f438985b4c1ec220a6723dca30f569d
Yulechka321 <yula.alipova@gmail.com>
2017-04-23 21:30:00 -0400

    automation added

---
+++ selenium/automation/fire.js
@@ -0,0 +1,94 @@
+var employees=[
+{"name":"Yulia",
+"team":"Incredibles"},
+{"name":"Bronya",
+"team":"Incredibles"},
+{"name":"Aksana",
+"team":"Operations"}]
+
+///for(var employee in employees){
+//	employees[employee]
+//}
+
+//var pass="success"
+
+//console.log(employees.length)
+
+var toFire=function(team){
+	if (employees[i]['team']=="Incredibles") {
+		return true
+	} else {
+		return false
+	}
+}
+
+for(var i=0; i<employees.length; i++)
+{
+
+//var team=employees[i]['team']
+//var isIncredible=true
+//var toFire=false
+// console.log(employees[i]['team']=="Incredibles")
+// var save=toFire(employees[i]['team'])
+
+console.log(employees[i]['name'])
+if (toFire(employees[i]['team'])) {
+	console.log('fired')
+} else {
+	console.log('saved')
+}
+ 
+// if (team=="Incredibles") {
+// 	console.log(true)}
+// 	else
+// 		{console.log(false)}
+// 	console.log(employees[i]['name'])
+// 	console.log()
+// 	}
+
+}	
+	
+
+
+
+// var team="Incredibles"
+// var toFire=false
+
+// if (team=="Incrediables") {
+// 	toFire=true
+// }
+
+
+// var day="Monday"
+
+// if (day=="Tuesday") {
+// 	var isToday=true
+// }
+// else {
+// 	var isToday=false
+
+// }
+
+// console.log(isToday)
+
+
+
+
+
+// // if (day=="Tuesday"){
+// 	console.log("today")
+// }
+
+//console.log(employees[0])
+
+
+//function(){ console.log ("name")}
+
+
+
+// function (fire) = {
+
+// if (employees[i] ['team'] =="Incredibles") fire (employees ["name"])
+// }
+
+console.log
